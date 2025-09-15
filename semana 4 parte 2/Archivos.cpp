0c869e7e9514882605a7c29d493b2cf94b07bc1e
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2025-09-15 15:51:00 -0300

    Update Archivos.cpp
    fwirte fread

---
+++ semana 4 parte 2/Archivos.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     FILE *p;
+     p=fopen("Alumnos.dat", "");
+     Alumno alu;
+     ///apertura para lectura =
+     ///rb: abre un archivo en modo lectura . NO crea archivos. solo abre archivos existentes
+     ///ab: abre un archivo en modo de escritura. si no existe el archivo lo crea.
+     ///wb: abre un archivo en modo escritura. Siempre crea un archivo vacio.
+
+     ///validacion
+     if (p ==nullptr){
+        cout<<"no se puedo abrir el archivo"<<endl;
+        return -1M;
+     }
+     //uso el archivo(lectura)
+     fread(&alu,sizeof alu ,1,p); /// LEE DEL DISCO Y SE COPIA EN LA MEMORIA
+     /* Direccion de memoria(ram) donde copiar la info desde el archivo(disco);
+     Cantidad de bytes que tiene un registro;
+     Cantidad de registros que quiero leer;
+     Cantidad de registros que quiero leer;
+     Vinculo entre mi programa y el archivo(punteroFILE sobre el que hice el fopen) */
+
+     //uso el archivo(escritura)
+     fwirte(&alu ,sizeof alu,1 , p) ///LEE DE LA MEMORIA Y COPIA EN EL DISCO
+     /*Desde donde queiro gurdar mi info;
+     La cantidad de bytes de mi regisgro
+     La camtodad de registros que quiero guardar en mi archivo.
+     El vinculo entre mi programa y el archivo*/
+
+
+     //termino de usar mi archivo
+     fclose(p);
+     return 0;
+}
