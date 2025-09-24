491b17eaeefac502bebbef1343c2564071c271a3
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2025-09-24 19:08:26 -0300

    cls materia

--- Todas las clases de los lunes hasta semana 6/AppGestion/clsMateria.cpp
+++ Todas las clases de los lunes hasta semana 6/AppGestion/clsMateria.cpp
@@ -1,12 +1,55 @@
-///Implementacion en los cpp
+#include <iostream>
+#include <cstring>
+#include "clsMateria.h"
 
+using namespace std;
 
-void setCodigo(int c);
-void setNombre(const char *n);
-void setCantidadDocentes(int cD);
-void setCantidadAlumnos (int cA);
+void Materia::Cargar(){
+    cout<<"INGRESE EL CODIGO: ";
+    cin>>codigo;
+    cout<<"INGRESE EL NOMBRE: ";
+    cin>>nombre;
+    cout<<"INGRESE LA CANTIDAD DE ALUMNOS: ";
+    cin>>cantidadAlumnos;
+    setCantidadAlumnos(cantidadAlumnos);
+    cout<<"INGRESE LA CANTIDAD DE DOCENTES: ";
+    cin>>cantidadDocentes;
+}
 
-int getCodigo();
-const char *getNombre():
-int getCantidadAlumnos();
-int getCantidadDocentes();
+void Materia::Mostrar(){
+    cout<<"CODIGO: "<<codigo<<endl;
+    cout<<"NOMBRE: "<<nombre<<endl;
+    cout<<"CANTIDAD ALUMNOS: "<<cantidadAlumnos<<endl;
+    cout<<"CANTIDAD DOCENTES: "<<cantidadDocentes<<endl;
+}
+
+void Materia::setCodigo(int c){
+    codigo = c;
+}
+void Materia::setNombre(const char *n){
+    strcpy(nombre, n);
+}
+void Materia::setCantidadAlumnos(int cA){
+    if(cA >= 0 and cA < 1000){
+        cantidadAlumnos = cA;
+    }else{
+        cantidadAlumnos = 0;
+    }
+}
+
+void Materia::setCantidadDocentes(int cD){
+    cantidadDocentes = cD;
+}
+
+int Materia::getCodigo(){
+    return codigo;
+}
+const char *Materia::getNombre(){
+    return nombre;
+}
+int Materia::getCantidadAlumnos(){
+    return cantidadAlumnos;
+}
+int Materia::getCantidadDocentes(){
+    return cantidadDocentes;
+}
