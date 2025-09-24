117a734a16da67b41a7d4451517840d88b59ddd1
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2025-09-24 19:09:44 -0300

    update main cpp

--- Todas las clases de los lunes hasta semana 6/AppGestion/main.cpp
+++ Todas las clases de los lunes hasta semana 6/AppGestion/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <cstring>
+#include "clsMateria.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+
+void mostrarMaterias(Materia *v,  int tam){
+    for(int i=0; i<tam; i++){
+        v[i].Mostrar();
+    }
+}
+
+void cargarMaterias(Materia *v, int tam){
+    for(int i=0; i<tam; i++){
+        v[i].Cargar();
+    }
+}
+class Alumno{
+
+};
+
+int main(){
+    Materia vecMateria[20];
+    Materia pepito;
+    Alumno pepita;
+    pepito.Cargar();
+    cout<<"SOY EL CONTENIDO DE PEPITO: "<<endl;
+    pepito.Mostrar();
+    cargarMaterias(vecMateria, 5);
+    pepita=pepito;
+    cout<<"======================"<<endl;
+    cout<<"SOY EL CONTENIDO DEL VECTOR DE MATERIAS: "<<endl;
+    mostrarMaterias(vecMateria,20);
     return 0;
 }
