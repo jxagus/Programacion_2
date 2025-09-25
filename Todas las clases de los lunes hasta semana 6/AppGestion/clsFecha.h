88a4cb36ff951e94bc7b26ee40f8409ddfc3a56a
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2025-09-25 14:09:18 -0300

    fecha

--- Todas las clases de los lunes hasta semana 6/AppGestion/clsFecha.h
+++ Todas las clases de los lunes hasta semana 6/AppGestion/clsFecha.h
@@ -1,16 +1,19 @@
-#ifndef CLSFECHA_H
-#define CLSFECHA_H
+#ifndef CLSFECHA_H_INCLUDED
+#define CLSFECHA_H_INCLUDED
 
-
-class clsFecha
-{
-    public:
-        clsFecha();
-        virtual ~clsFecha();
-
-    protected:
-
+class Fecha{
     private:
+        int dia, mes, anio;
+    public:
+        Fecha(int d=1, int m=1, int a=1900);
+        void setDia(int);
+        void setMes(int);
+        void setAnio(int);
+        int getDia();
+        int getMes();
+        int getAnio();
+        void Cargar();
+        void Mostrar();
 };
 
-#endif // CLSFECHA_H
+#endif // CLSFECHA_H_INCLUDED
