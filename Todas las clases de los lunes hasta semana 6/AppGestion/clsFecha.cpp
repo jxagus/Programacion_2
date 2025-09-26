3ed146dac939da1f9c0edba58fb6bb87297e5c59
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2025-09-26 20:50:58 -0300

    cpp fecha

--- Todas las clases de los lunes hasta semana 6/AppGestion/clsFecha.cpp
+++ Todas las clases de los lunes hasta semana 6/AppGestion/clsFecha.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
-using namespace std;
-
-int main(){
-
-     return 0;
-}
 #include "clsFecha.h"
 
-clsFecha::clsFecha()
-{
-    //ctor
-}
+using namespace std;
 
-clsFecha::~clsFecha()
-{
-    //dtor
+Fecha::Fecha(int d, int m, int a){
+    dia = d;
+    mes = m;
+    anio = a;
+}
+void Fecha::setDia(int d){
+    dia = d;
+}
+void Fecha::setMes(int m){
+    mes = m;
+}
+void Fecha::setAnio(int a){
+    anio = a;
+}
+int Fecha::getDia(){
+    return dia;
+}
+int Fecha::getMes(){
+    return mes;
+}
+int Fecha::getAnio(){
+    return anio;
+}
+void Fecha::Cargar(){
+    cout<<"DIA: ";
+    cin>>dia;
+    cout<<"MES: ";
+    cin>>mes;
+    cout<<"ANIO: ";
+    cin>>anio;
+}
+void Fecha::Mostrar(){
+    cout<<dia<<"/"<<mes<<"/"<<anio;
 }
